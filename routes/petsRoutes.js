c7c3cc5946d172e7c84ddfe2d40b8e0c10394abc
DanielBarragan96 <ie702554@iteso.mx>
2020-12-05 23:14:10 -0600

    added parameter to get pet using id_pet

--- routes/petsRoutes.js
+++ routes/petsRoutes.js
@@ -30,6 +30,11 @@ router.get('/', async (req, res) => {
     } else if (req.query.not_owner_id) {
         pets = pets.filter(pet => (pet.owner_id != req.query.not_owner_id));
     }
+
+    if (req.query.id_pet) {
+        pets = pets.filter(pet => (pet._id == req.query.id_pet));
+    }
+
     if (req.query.name) {
         let nom = (req.query.name) ? req.query.name : '';
         pets = pets.filter((ele, index, arr) => {
